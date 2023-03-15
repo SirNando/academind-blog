a0190d3ef76ba5e8e12eaf29586d53c61c247dec
SirNando <matiasnandin@hotmail.com>
2023-03-15 09:38:25 -0300

    Modified files

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     database: 'blog',
     user: 'root',
-    password: 'iMatinandinFTW13'
+    password: '[REDACTED]'
 });
 
 module.exports = pool;
